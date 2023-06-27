40ada32f6e8a6fa400f14e2cd7d5d3b870f97a46
kuanyi0226 <kuanyi0226@gmail.com>
2023-06-28 04:09:36 +0800

    beta V0.2.0

--- lib/materials/MyText.dart
+++ lib/materials/MyText.dart
@@ -3,6 +3,6 @@ const APPNAME_EN = 'YUKI CLUB';
 const APPNAME_JP = 'ゆき クラブ';
 
 const UPDATE_CONTENT = [
-  '--beta V0.2.0--\nfirst published version\nadd: More about settings, AD system, 今日の曲',
+  '--beta V0.2.0-- 2023/6/28\nfirst released version\nadd: More about settings, AD system, 今日の曲',
   '--beta V0.1.0--\nadd: Search Songs, yakai page, other functions, message board\nfix: comment bug',
 ];
